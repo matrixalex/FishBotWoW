3dd1eeebc2a3fc647c206c19211d79f4d8d0d080
matrixalex <matrixalex96@gmail.com>
2020-07-22 11:06:10 +0300

    Инструкция к боту

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ import cv2
 import numpy as np
 
 fish_count = 0
-max_fish_count = 300
-threshold = 0.5
-action_diff = 6
-x_correction = 15
-y_correction = 25
-fish_ability_x = 750
-fish_ability_y = 785
+max_fish_count = 300  # Ограничивающая переменная
+threshold = 0.5  # Переменная, используемая для поиска поплавка на скриншоте игры
+action_diff = 6  # Разность
+x_correction = 15  # Коррекция по x для наведения на поплавок
+y_correction = 25  # Коррекцияя по y для наведения на поплавок
+fish_ability_x = 750  # Координаты навыка рыбалки по x на панели WoW (задать руками методом тыка)
+fish_ability_y = 785  # Координаты навыка рыбалки по y на панели WoW (задать руками методом тыка)
+window_rectangle = (0, 0, 1100, 800)  # Координаты прямоугольника окна, с него делается скриншот
 focused = False
-bobber_template = cv2.imread('src/bobber1.jpg', 0)
+bobber_template_path = 'src/bobber1.jpg'  # Путь до файла шаблона поплавка
+bobber_template = cv2.imread(bobber_template_path, 0)
 w, h = bobber_template.shape[::-1]
 # print(w, h)
 
 for _ in range(max_fish_count):
     print('Fishing ' + str(fish_count))
-    screen = ImageGrab.grab(bbox=(0, 0, 1100, 800))
+    screen = ImageGrab.grab(bbox=window_rectangle)
     screen.save('screen.jpg')
     img = cv2.cvtColor(cv2.imread('screen.jpg'), cv2.COLOR_BGR2GRAY)
     res = cv2.matchTemplate(img, bobber_template, cv2.TM_CCOEFF_NORMED)
@@ -50,10 +52,11 @@ for _ in range(max_fish_count):
                 if not current_mean:
                     current_mean = mean
                 diff = abs(current_mean - mean)
+                # Здесь отслеживаем показания которые принтуются в консоль, чтобы установить переменной
+                # action_diff нужное вам значение
                 print(diff)
                 if diff > action_diff:
                     action = True
-                # print(diff)
                 time_count += 0.2
                 # print(time_count)
                 time.sleep(0.2)
